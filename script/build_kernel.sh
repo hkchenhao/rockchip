4a3df792cdc78c33e7bdccc2a515d2778ad0c6e4
chenhao222 <chenhao222@meituan.com>
2026-02-27 19:59:28 +0800

    update

--- script/build_kernel.sh
+++ script/build_kernel.sh
@@ -7,7 +7,7 @@ make ARCH=arm64 CROSS_COMPILE=aarch64-none-linux-gnu- rockchip_linux_defconfig
 make ARCH=arm64 CROSS_COMPILE=aarch64-none-linux-gnu- menuconfig
 make ARCH=arm64 savedefconfig && mv defconfig arch/arm64/configs/rk3399_linux_defconfig
 make ARCH=arm64 CROSS_COMPILE=aarch64-none-linux-gnu- rk3399-eaidk-610.img KCFLAGS="-Wno-error" -j16
-make ARCH=arm64 CROSS_COMPILE=aarch64-linux-gnu- modules KCFLAGS="-Wno-error" -j16
-make ARCH=arm64 CROSS_COMPILE=aarch64-linux-gnu- modules_install INSTALL_MOD_PATH=../build/rootfs
+make ARCH=arm64 CROSS_COMPILE=aarch64-none-linux-gnu- modules KCFLAGS="-Wno-error" -j16
+make ARCH=arm64 CROSS_COMPILE=aarch64-none-linux-gnu- modules_install INSTALL_MOD_PATH=../build/rootfs
 mv boot.img ../build/kernel.img
 cd -
