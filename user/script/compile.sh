36bd162ba92d699031b06db880e891823712cc80
chenhao222 <chenhao222@meituan.com>
2026-02-28 16:36:05 +0800

    update

--- user/script/compile.sh
+++ user/script/compile.sh
@@ -13,8 +13,8 @@ git clone https://github.com/rockchip-linux/kernel.git kernel -b develop-6.1 --s
 cd loader
 ./tools/boot_merger ./RKBOOT/RK3399MINIALL.ini
 ./tools/trust_merger ./RKTRUST/RK3399TRUST.ini
-mv rk3399_loader_*.bin ../build/loader.bin
-mv trust.img ../build/trust.img
+cp rk3399_loader_*.bin ../user/build/loader.bin
+cp trust.img ../user/build/trust.img
 cd -
 
 cd uboot
@@ -24,7 +24,7 @@ make CROSS_COMPILE=aarch64-none-linux-gnu- menuconfig
 make savedefconfig && mv defconfig ./configs/rk3399_defconfig
 make CROSS_COMPILE=aarch64-none-linux-gnu- KCFLAGS="-Wno-error" -j16
 ./../loader/tools/loaderimage --pack --uboot u-boot.bin uboot.img 0x200000 --size 2048 2
-mv uboot.img ../build/uboot.img
+cp uboot.img ../user/build/uboot.img
 cd -
 
 # Ref: https://www.cnblogs.com/solo666/p/15953768.html https://blog.csdn.net/fhy00229390/article/details/112980643)
@@ -35,6 +35,6 @@ make ARCH=arm64 CROSS_COMPILE=aarch64-none-linux-gnu- menuconfig
 make ARCH=arm64 savedefconfig && mv defconfig arch/arm64/configs/rk3399_linux_defconfig
 make ARCH=arm64 CROSS_COMPILE=aarch64-none-linux-gnu- rk3399-eaidk-610.img KCFLAGS="-Wno-error" -j16
 make ARCH=arm64 CROSS_COMPILE=aarch64-none-linux-gnu- modules KCFLAGS="-Wno-error" -j16
-make ARCH=arm64 CROSS_COMPILE=aarch64-none-linux-gnu- modules_install INSTALL_MOD_PATH=../build/rootfs
-mv boot.img ../build/kernel.img
+make ARCH=arm64 CROSS_COMPILE=aarch64-none-linux-gnu- modules_install INSTALL_MOD_PATH=../user/build/rootfs
+cp boot.img ../user/build/kernel.img
 cd -
